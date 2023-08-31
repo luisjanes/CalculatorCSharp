0495c4fec94a75c4b629254172a2dd37281e8937
Luis Felipe da Costa Janes <luisf.janes@gmail.com>
2023-08-31 15:57:00 -0300

    style: improving the system, changing the way it init

--- Program.cs
+++ Program.cs
@@ -4,63 +4,90 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        string mathContinue = "s";
-        while (mathContinue != "n")
+        Menu();
+    }
+
+    static void Menu()
+    {
+        Console.Clear();
+        Console.WriteLine("Informe a operação matemática desejada: ");
+        Console.WriteLine("/ para divisão");
+        Console.WriteLine("* para multiplicação");
+        Console.WriteLine("- para subtração");
+        Console.WriteLine("+ para soma");
+        Console.WriteLine("5 para sair do programa");
+        string mathOperator = Console.ReadLine();
+        float resultOf;
+
+        switch (mathOperator)
         {
-
-            Console.WriteLine("Informe o primeiro número: ");
-            float numberOne = float.Parse(Console.ReadLine());
-            Console.WriteLine("Informe o segundo número: ");
-            float numberTwo = float.Parse(Console.ReadLine());
-            Console.WriteLine("Informe a operação matemática desejada: ");
-            Console.WriteLine("/ para divisão");
-            Console.WriteLine("* para multiplicação");
-            Console.WriteLine("- para subtração");
-            Console.WriteLine("+ para soma");
-            string mathOperator = Console.ReadLine();
-            float resultOf;
-
-            switch (mathOperator)
-            {
-                case "+":
-                    OperatorPlus(numberOne, numberTwo);
-                    break;
-                case "-":
-                    OperatorSubtraction(numberOne, numberTwo);
-                    break;
-                case "*":
-                    OperatorMultiply(numberOne, numberTwo);
-                    break;
-                case "/":
-                    OperatorDivide(numberOne, numberTwo);
-                    break;
-                default:
-                    Console.WriteLine("Nenhuma opção selecionada.");
-                    break;
-            }
-            Console.WriteLine("Deseja continuar? s para sim e n para não");
-            mathContinue = Console.ReadLine();
+            case "+":
+                OperatorPlus();
+                break;
+            case "-":
+                OperatorSubtraction();
+                break;
+            case "*":
+                OperatorMultiply();
+                break;
+            case "/":
+                OperatorDivide();
+                break;
+            case "5":
+                System.Environment.Exit(0);
+                break;
+            default:
+                Menu();
+                break;
         }
     }
-
-    static void OperatorPlus(float numberOne, float numberTwo)
+    static void OperatorPlus()
     {
+        Console.Clear();
+        Console.WriteLine("Informe o primeiro número: ");
+        float numberOne = float.Parse(Console.ReadLine());
+        Console.WriteLine("Informe o segundo número: ");
+        float numberTwo = float.Parse(Console.ReadLine());
         float result = numberOne + numberTwo;
         Console.WriteLine($"O resultado da soma é {result}");
+        Console.ReadKey();
+        Menu();
     }
-    static void OperatorSubtraction(float numberOne, float numberTwo)
+    static void OperatorSubtraction()
     {
+        Console.Clear();
+        Console.WriteLine("Informe o primeiro número: ");
+        float numberOne = float.Parse(Console.ReadLine());
+        Console.WriteLine("Informe o segundo número: ");
+        float numberTwo = float.Parse(Console.ReadLine());
         float result = numberOne - numberTwo;
         Console.WriteLine($"O resultado da subtração é {result}");
+        Console.ReadKey();
+        Menu();
     }
-    static void OperatorMultiply(float numberOne, float numberTwo)
+    static void OperatorMultiply()
     {
+        Console.Clear();
+        Console.WriteLine("Informe o primeiro número: ");
+        float numberOne = float.Parse(Console.ReadLine());
+        Console.WriteLine("Informe o segundo número: ");
+        float numberTwo = float.Parse(Console.ReadLine());
         float result = numberOne * numberTwo;
         Console.WriteLine($"O resultado da multiplicação é {result}");
+        Console.ReadKey();
+        Menu();
     }
-    static void OperatorDivide(float numberOne, float numberTwo)
+    static void OperatorDivide()
     {
+        Console.Clear();
+        Console.WriteLine("Informe o primeiro número: ");
+        float numberOne = float.Parse(Console.ReadLine());
+        Console.WriteLine("Informe o segundo número: ");
+        float numberTwo = float.Parse(Console.ReadLine());
         float result = numberOne / numberTwo;
         Console.WriteLine($"O resultado da divisão é {result}");
+        Console.ReadKey();
+        Menu();
     }
+
 }
